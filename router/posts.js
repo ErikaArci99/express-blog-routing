083f13dafda9d913e77842a0f9ec656f9e9a3779
ErikaArci99 <erikaarci1999@outlook.it>
2025-04-17 15:27:10 +0200

    [DONE] corretti alcuni errori e esercizio terminato

--- router/posts.js
+++ router/posts.js
@@ -4,34 +4,34 @@ const router = express.Router();
 // creo le nuove rotte
 
 // index
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
     res.send('Lista dei post');
-  });
+});
 
 // show
-app.get('/:id', (req, res) => {
-    res.send('Dettagli del post' + req.params.id);
-  });
+router.get('/:id', (req, res) => {
+    res.send('Dettagli del post ' + req.params.id);
+});
 
 // store
-app.post('/', (req, res) => {
+router.post('/', (req, res) => {
     res.send('Creazione nuovo post');
-  });
+});
 
-//  update
-app.put('/:id', (req, res) => {
-    res.send('Modifica integrale del post' + req.params.id);
-  });
+// update
+router.put('/:id', (req, res) => {
+    res.send('Modifica integrale del post ' + req.params.id);
+});
 
 // modify
-app.patch('/:id', (req, res) => {
-    res.send('Modifca parziale del post' + req.params.id);
-  });
+router.patch('/:id', (req, res) => {
+    res.send('Modifica parziale del post ' + req.params.id);
+});
 
 // destroy
-app.delete('/:id', (req, res) => {
-    res.send('Eliminazione del post' + req.params.id);
-  });
+router.delete('/:id', (req, res) => {
+    res.send('Eliminazione del post ' + req.params.id);
+});
 
 //  esporto le rotte in app.js
 module.exports = router;
