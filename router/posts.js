5bc4fe4e3591e15d1bf4030b9a9053557bc5f9c0
ErikaArci99 <erikaarci1999@outlook.it>
2025-04-17 15:18:28 +0200

    [UPD] creata rotta index

--- router/posts.js
+++ router/posts.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+// creo le nuove rotte
 
+// index
+router.get('/', (req, res) => {
+    res.send('Lista dei post');
+  });
 
 
 
